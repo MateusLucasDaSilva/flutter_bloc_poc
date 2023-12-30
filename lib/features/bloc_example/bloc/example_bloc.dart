493fd10b49dfaf4fd410b4949ee9b3997822df67
MateusLucasDaSilva <mateuslukas505@gmail.com>
2023-12-30 14:29:28 -0300

    implementei a inserção numa list5a de um estado di bloc

--- lib/features/bloc_example/bloc/example_bloc.dart
+++ lib/features/bloc_example/bloc/example_bloc.dart
@@ -9,12 +9,39 @@ part 'example_state.dart';
 class ExampleBloc extends Bloc<ExampleEvent, ExampleState> {
   ExampleBloc() : super(ExampleStateInitial()) {
     on<ExampleFindNameEvent>(_findNames);
+    on<ExampleRemoveNameEvent>(_removeName);
+    on<ExampleAddNameEvent>(_addName);
+  }
+
+  FutureOr<void> _removeName(
+    ExampleRemoveNameEvent event,
+    Emitter<ExampleState> emit,
+  ) {
+    final ExampleState currentState = state;
+
+    if (currentState is ExampleStateData) {
+      currentState.names.remove(event.name);
+      emit(ExampleStateData(names: currentState.names));
+    }
+  }
+
+  FutureOr<void> _addName(
+    ExampleAddNameEvent event,
+    Emitter<ExampleState> emit,
+  ) {
+    final ExampleState currentState = state;
+
+    if (currentState is ExampleStateData) {
+      currentState.names.insert(0, event.name);
+      emit(ExampleStateData(names: currentState.names));
+    }
   }
 
   FutureOr<void> _findNames(
     ExampleFindNameEvent event,
     Emitter<ExampleState> emit,
   ) async {
+    await Future.delayed(const Duration(seconds: 3));
     final names = [
       'Mateus',
       'Lucas',
--- lib/features/bloc_example/bloc_example_page.dart
+++ lib/features/bloc_example/bloc_example_page.dart
@@ -11,25 +11,102 @@ class BlocExamplePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Bloc Example'),
       ),
-      body: BlocBuilder<ExampleBloc, ExampleState>(
-        builder: (context, state) {
-          if (state is ExampleStateData) {
-            return ListView(
-              children: state.names
-                  .map(
-                    (e) => ListTile(
-                      title: Text(e),
-                    ),
-                  )
-                  .toList(),
+      floatingActionButton: IconButton(
+        onPressed: () => _addName(context),
+        icon: const Icon(Icons.add),
+      ),
+      body: BlocListener<ExampleBloc, ExampleState>(
+        listenWhen: (previous, current) {
+          return previous is ExampleStateInitial && current is ExampleStateData;
+        },
+        listener: (context, state) {
+          if (state is ExampleStateInitial) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Carregando'),
+              ),
             );
           }
-          if (state is ExampleStateInitial) {
-            return const Center(child: CircularProgressIndicator());
+          if (state is ExampleStateData) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Carregado !'),
+              ),
+            );
           }
-          return const SizedBox.shrink();
         },
+        child: Column(
+          children: [
+            BlocSelector<ExampleBloc, ExampleState, bool>(
+              selector: _isInitialState,
+              builder: (context, isLoading) {
+                if (isLoading) {
+                  return const Expanded(
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+            BlocConsumer<ExampleBloc, ExampleState>(
+              listener: (context, state) {},
+              builder: (_, state) {
+                if (state is ExampleStateData) {
+                  return SizedBox(
+                    height: 300,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: state.names.length,
+                      itemBuilder: (context, index) {
+                        final String name = state.names[index];
+                        return ListTile(
+                          onTap: () {
+                            context
+                                .read<ExampleBloc>()
+                                .add(ExampleRemoveNameEvent(name: name));
+                          },
+                          title: Text('# $name'),
+                        );
+                      },
+                    ),
+                  );
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+            BlocBuilder<ExampleBloc, ExampleState>(
+              builder: (context, state) {
+                if (state is ExampleStateData) {
+                  return SizedBox(
+                    height: 300,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: state.names.length,
+                      itemBuilder: (context, index) => ListTile(
+                        title: Text('& ${state.names[index]}'),
+                      ),
+                    ),
+                  );
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void _addName(BuildContext context) {
+    context.read<ExampleBloc>().add(ExampleAddNameEvent(name: 'Mateus'));
+  }
+
+  bool _isInitialState(ExampleState state) {
+    if (state is ExampleStateInitial) {
+      return true;
+    }
+    return false;
   }
 }
